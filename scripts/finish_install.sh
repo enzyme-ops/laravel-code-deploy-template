637a8678699e7c9c633abc543754b7ea5055dcc5
Tristan Strathearn <r3oath@gmail.com>
2016-12-19 12:49:47 +1000

    Update finish_install.sh

--- scripts/finish_install.sh
+++ scripts/finish_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Copy the production environment file from S3 to the local installation
-aws s3 cp s3://XXX/env/production.env /var/www/snapshot/.env
+aws s3 cp s3://ergon-ebill/env/production.env /var/www/snapshot/.env
 
 # Setup the various file and folder permissions for Laravel
 chown -R ubuntu:www-data /var/www/snapshot
@@ -11,8 +11,11 @@ chgrp -R www-data /var/www/snapshot/storage /var/www/snapshot/bootstrap/cache
 chmod -R ug+rwx /var/www/snapshot/storage /var/www/snapshot/bootstrap/cache
 
 # Clear any previous cached views and optimize the application
+php /var/www/snapshot/artisan cache:clear
 php /var/www/snapshot/artisan view:clear
+php /var/www/snapshot/artisan config:cache
 php /var/www/snapshot/artisan optimize
+php /var/www/snapshot/artisan route:cache
 
 # Reload PHP-FPM so that any cached code is subsequently refreshed
 service php7.0-fpm reload
